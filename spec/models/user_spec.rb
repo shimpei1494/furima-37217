df15710b81a39d65013778d5499586144b9f8f53
shimpei1494 <one1218all38@icloud.com>
2022-03-12 08:27:34 +0900

    user_spec.rbにテストの項目一覧を記述した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it '全ての項目に正しく入力があれば登録できる' do
+
+      end
+    end
+    context '新規登録できない場合' do
+      it 'nicknameが存在しないと登録できない' do
+
+      end
+
+      it 'emailが存在しないと登録できない' do
+
+      end
+      it 'emailに@が含まれていないと登録できない' do
+      end
+
+      it 'emailが一意性でないと登録できない' do
+      end
+
+      it 'passwordが存在しないと登録できない' do
+        
+      end
+
+      it 'passwordは5文字以下では登録できない' do
+      end
+
+      it 'passwordは半角英数字混合でないと登録できない' do
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+
+      end
+
+      it 'family_nameが存在しないと登録できない' do
+      end
+
+      it 'family_nameが全角（漢字・ひらがな・カタカナ）以外では登録できない'
+      end
+
+      it 'first_nameが存在しないと登録できない' do
+      end
+
+      it 'first_nameが全角（漢字・ひらがな・カタカナ）以外では登録できない'
+      end
+
+      it 'family_name_kanaが存在しないと登録できない' do
+      end
+
+      it 'family_name_kanaが全角（カタカナ）以外では登録できない' do
+      end
+
+      it 'first_name_kanaが存在しないと登録できない' do
+      end
+
+      it 'first_name_kanaが全角（カタカナ）以外では登録できない' do
+      end
+
+      it 'birth_dayが存在しないと登録できない' do
+        
+      end
+
+    end
+  end
 end
