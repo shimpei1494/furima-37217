014e669e77b361f75b4d5b82fa9f428051bb5a0d
shimpei1494 <one1218all38@icloud.com>
2022-03-13 18:31:57 +0900

    Itemモデルにバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,15 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee_task
   belongs_to :prefecture
   belongs_to :delivery_day
-
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :delivery_fee_task_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :delivery_day_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :price, presence: true, numericality: {only_integer: true, greater_than: 299, less_than: 10000000}, format: { with: /\A[0-9]+\z/, message: '半角数字で入力してください'}
+
 end
