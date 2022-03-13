67333af6e191f410ff5329b11656bc6992368e63
shimpei1494 <one1218all38@icloud.com>
2022-03-13 20:22:55 +0900

    ItemモデルにActiveStorageのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
   belongs_to :user
+  has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
