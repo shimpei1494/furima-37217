691af13a2c03b508444e40cd389b0c2fa7d3ce59
shimpei1494 <one1218all38@icloud.com>
2022-03-13 17:08:01 +0900

    Itemモデル、Userモデルにアソシエーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee_task
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  belongs_to :user
 end
