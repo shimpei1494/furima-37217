227540c3a39b7059c98f0eeea8f706a97f86300b
shimpei1494 <one1218all38@icloud.com>
2022-03-24 21:47:12 +0900

    購入ページの表示実装
    購入検討商品の情報を表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,9 @@
 class PurchasesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseDeliveryAddress.new
+  end
+
+  def create
+  end
 end
