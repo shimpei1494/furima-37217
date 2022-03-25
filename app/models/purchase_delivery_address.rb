0ad02ef108a32825fe98c31e018e9b576e2a3e2a
shimpei1494 <one1218all38@icloud.com>
2022-03-25 20:37:04 +0900

    バリデーションの記述

--- app/models/purchase_delivery_address.rb
+++ app/models/purchase_delivery_address.rb
@@ -1,4 +1,14 @@
 class PurchaseDeliveryAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "10桁か11桁の半角数値をハイフン(-)なしで入力してください"}
+  end
+  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 end
