f03ef6ec5db465b3dc1b43083b79272698b30756
shimpei1494 <one1218all38@icloud.com>
2022-03-26 10:10:47 +0900

    createメソッドに記述
    プラス
    indexの中身を修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,23 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @purchase_address = PurchaseDeliveryAddress.new
+    @purchase_delivery_address = PurchaseDeliveryAddress.new
   end
 
   def create
+    @purchase_delivery_address = PurchaseDeliveryAddress.new(purchase_params)
+    if @purchase_delivery_address.valid?
+      @purchase_delivery_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
