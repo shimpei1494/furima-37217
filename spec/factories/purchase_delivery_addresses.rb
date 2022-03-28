ed9578f5fc28051b1d76c8a433be3b220820b19e
shimpei1494 <one1218all38@icloud.com>
2022-03-28 16:45:59 +0900

    factorybotの作成

--- spec/factories/purchase_delivery_addresses.rb
+++ spec/factories/purchase_delivery_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_delivery_address do
-    
+
+    token {'token'}
+    post_code {'000-0000'}
+    prefecture_id {rand(2..48)}
+    city {'city'}
+    address {'address'}
+    building_name {'building'}
+    phone_number {'01234567891'} #10桁か11桁
+
   end
 end
