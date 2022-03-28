9f6b1ec93d9fd0c509c73550815e8e449dbd5782
shimpei1494 <one1218all38@icloud.com>
2022-03-28 11:52:11 +0900

    tokenコントローラで取得できるように

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -24,6 +24,6 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
